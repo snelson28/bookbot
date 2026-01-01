a8d1143928d6e05368c29092a439ea18778c5590
Sam Nelson <samnelson2820@gmail.com>
2025-12-31 19:45:41 -0600

    add main.py - count words

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(filepath):
+    """Reads the contents of the file and returns it as a string."""
+    with open(filepath, 'r', encoding='utf-8') as file:
+        contents = file.read()
+    return contents
+
+def count_words(text):
+    """Returns the number of words in a given text string."""
+    words = text.split()
+    return len(words)
+
+def main():
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+
+if __name__ == "__main__":
+    main()
